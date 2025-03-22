a7e2129465f4b0b0792d51fb123d6227e052ab57
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-03-22 20:07:16 +0530

    Added search functionality

--- src/components/MoviePage/index.jsx
+++ src/components/MoviePage/index.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 
+import { useFetchMovies } from "hooks/reactQuery/useMoviesApi";
 import { Search } from "neetoicons";
 import { Input } from "neetoui";
 
 const MoviePage = () => {
   const [searchText, setSearchText] = useState("");
 
+  const searchParams = { s: searchText };
+
+  const { data: { Search: movies = [] } = {} } = useFetchMovies(searchParams);
+  console.log(movies);
+
   return (
-    <div className="p-6">
+    <div className="bg-red-100 p-6">
       <div className="mx-auto mb-8 max-w-2xl">
         <Input
           className="rounded-lg border border-[#ddd]"
           placeholder="Search movie or series..."
           prefix={<Search />}
-          type="text"
+          type="search"
           value={searchText}
           onChange={e => setSearchText(e.target.value)}
         />
       </div>
+      <div>{JSON.stringify(movies)}</div>
     </div>
   );
 };
