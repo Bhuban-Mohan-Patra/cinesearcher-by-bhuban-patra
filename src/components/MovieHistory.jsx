5f77a015cf5c7e7d784fe9a42f33966ef582562e
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-03-26 00:19:17 +0530

    Added view history deletion and clear with confirmation

--- src/components/MovieHistory.jsx
+++ src/components/MovieHistory.jsx
@@ -1,13 +1,20 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import classNames from "classnames";
+import { Delete } from "neetoicons";
+import { Alert, Button } from "neetoui";
 import { useTranslation } from "react-i18next";
 import useViewHistoryStore from "stores/useViewHistoryStore";
 
 const ViewHistory = () => {
+  const [isClearAlertOpen, setIsClearAlertOpen] = useState(false);
   const { t } = useTranslation();
   const movies = useViewHistoryStore(state => state.movies);
   const selectedMovie = useViewHistoryStore(state => state.selectedMovie);
+
+  const removeMovie = useViewHistoryStore(state => state.removeMovie);
+  const clearHistory = useViewHistoryStore(state => state.clearHistory);
+
   const historyContainerRef = useRef(null);
   const movieItemRefs = useRef({});
 
@@ -19,11 +26,29 @@ const ViewHistory = () => {
     }
   }, [selectedMovie]);
 
+  const handleClearHistory = () => {
+    setIsClearAlertOpen(true);
+  };
+
+  const confirmClearHistory = () => {
+    clearHistory();
+    setIsClearAlertOpen(false);
+  };
+
   return (
-    <div className="h-screen w-full overflow-scroll rounded-lg bg-white p-4 shadow-lg">
-      <h2 className="mb-4 text-center text-lg font-bold">
-        {t("viewHistory.title")}
-      </h2>
+    <div className="h-screen max-h-screen w-full overflow-y-scroll rounded-lg bg-white p-4 shadow-lg">
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-center text-lg font-bold">
+          {t("viewHistory.title")}
+        </h2>
+        <Button
+          className="font-semibold text-red-600 "
+          disabled={movies.length === 0}
+          label="Clear all"
+          style="tertiary"
+          onClick={handleClearHistory}
+        />
+      </div>
       <div
         className="max-h-[70vh] space-y-2 overflow-y-auto"
         ref={historyContainerRef}
@@ -33,16 +58,30 @@ const ViewHistory = () => {
             key={`${movie.Title}-${index}`}
             ref={el => (movieItemRefs.current[movie.Title] = el)}
             className={classNames(
-              "rounded-lg p-3 text-center transition-colors",
+              "flex items-center justify-between rounded-lg px-4 py-3 transition-colors",
               movie === selectedMovie
                 ? "bg-blue-600 text-white"
                 : "bg-blue-100 text-black"
             )}
           >
-            {movie.Title}
+            <span>{movie.Title}</span>
+            <Delete
+              className="cursor-pointer"
+              onClick={() => removeMovie(movie.imdbID)}
+            />
           </div>
         ))}
       </div>
+      <Alert
+        cancelButtonLabel="Cancel"
+        closeOnOutsideClick={false}
+        isOpen={isClearAlertOpen}
+        message="Are you sure you want to clear all history?"
+        submitButtonLabel="Clear"
+        title="Clear view history"
+        onClose={() => setIsClearAlertOpen(false)}
+        onSubmit={confirmClearHistory}
+      />
     </div>
   );
 };
